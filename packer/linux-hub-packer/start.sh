dcc40c564ecb98048ab96294a845c12867d4af3a
Alex Romanov <alex@romanov.ws>
2016-08-22 14:19:09 -0700

    Update start.sh

--- packer/linux-hub-packer/start.sh
+++ packer/linux-hub-packer/start.sh
@@ -3,4 +3,5 @@ IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 java -DipAddress="$IP" \
   -DpropertyFileLocation="/aws.properties" \
   -cp target/automation-grid.jar org.openqa.grid.selenium.GridLauncher \
-  -role hub -servlets "com.rmn.qa.servlet.AutomationTestRunServlet","com.rmn.qa.servlet.StatusServlet" &
+  -role hub -servlets "com.rmn.qa.servlet.AutomationTestRunServlet","com.rmn.qa.servlet.StatusServlet" \
+  > /opt/SeleniumGridScaler/log/stdouterr 2>&1 &
